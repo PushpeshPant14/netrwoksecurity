3b1c2d9f8b9e3984a99eef7b78ad59841f773eab
Pushpesh Pant <pushpeshpant19@gmail.com>
2026-07-05 17:45:11 +0530

    ETL Pipeline : Implemented - v1

---
+++ test_mongodb.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import pymongo
+from networksecurity.logging.logger import logger
+from networksecurity.exception.exception import NetworkSecurityException
+
+import certifi
+ca = certifi.where()
